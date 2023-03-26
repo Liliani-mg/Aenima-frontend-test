68e50cea352c0a581266b2d3ae06f209cc7c7213
Irupe <lilianirupemeza@gmail.com>
2023-03-26 09:20:18 -0300

    fix Footer styles margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 function Footer() {
+
+  const isMobile = useMediaQuery({ query: "(max-width: 900px)" });
+
   return (
-    <footer class="d-flex flex-wrap justify-content-between align-items-center  my-4 border-top bg-dark pt-4 p-5 ">
-      <div class="col-md-2 d-flex align-items-center ms-4 mt-0 mb-0">
+    <footer class={isMobile?"d-flex flex-wrap justify-content-between align-items-center border-top bg-dark px-2 py-2 ":"d-flex flex-wrap justify-content-between align-items-center border-top bg-dark pt-4 p-5 "}>
+      <div class="col-md-2 d-flex align-items-center ms-4 mt-1 mb-0 ">
         <span class="mb-2 mt-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
           <img src="src\assets\logo\logo_02.png" alt="" />
         </span>
       </div>
-      <div class="ms-5 mt-0 mb-0">
+      <div class="my-1 mx-4">
         <ul class="nav  justify-content-end list-unstyled d-flex flex-row">
           <li class="ms-2 p-3 pt-0 pb-0">
             <img src="src\assets\iconos\instagram.png" alt="instagram" />
