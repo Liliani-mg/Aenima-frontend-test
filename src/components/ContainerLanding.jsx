d2876dea7c894d81252e1af0336a8d6775fb4c0a
Irupe <lilianirupemeza@gmail.com>
2023-03-26 09:22:03 -0300

    fix Landing content styles

--- src/components/ContainerLanding.jsx
+++ src/components/ContainerLanding.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 import Footer from './Footer'
 import NavBar from './NavBar'
 import SecondContent from './SecondContent'
 import TopContent from './TopContent'
 
 function ContainerLanding() {
+
+  const isMobile = useMediaQuery({ query: "(max-width: 900px)" });
   return (
     <div id='container' class="">
       <NavBar/>
+      <div class={!isMobile&&"mx-5 px-5"}>
       <TopContent/>
       <SecondContent/>
-      <Footer/>
+      
+      </div>
+     <Footer/> 
     </div>
   )
 }
